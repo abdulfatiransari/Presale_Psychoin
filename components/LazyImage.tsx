6f3b07dbe20a9e7d67e3aff83e1bdcc1344a7c2f
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2024-04-16 12:53:15 +0500

    fixed: remove nextui skeleton

--- components/LazyImage.tsx
+++ components/LazyImage.tsx
@@ -1,13 +1,13 @@
-import { Card, Skeleton } from "@nextui-org/react";
 import Image, { ImageProps } from "next/image";
 import React, { useState } from "react";
 
 const LazyImage = (props: ImageProps) => {
   const [loading, setLoading] = useState(true);
   return (
-    <Skeleton isLoaded={!loading} className="bg-gray-600">
-      <Image onLoad={() => setLoading(false)} {...props} />
-    </Skeleton>
+    <div className="relative">
+      <Image {...props} className={props.className + (loading ? " !opacity-0" : "")} onLoad={() => setLoading(false)}  />
+    {loading && <div className="animate-pulse top-0 absolute w-full h-full bg-slate-700"></div>}
+    </div>
   );
 };
 
