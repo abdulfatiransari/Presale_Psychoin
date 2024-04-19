3e2ffab978ecbb5969b1f10aa94b4777e4c0b1a0
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2024-04-19 17:03:29 +0500

    fixed: stripe issue

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -55,7 +55,7 @@ const CheckoutForm = () => {
     <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center py-20">
       <div className="bg-[#fff]/65 shadow-lg flex flex-col justify-center px-10 py-10 rounded-xl">
       <PaymentElement />
-      <Button isLoading={loading} className="bg-[#6D00CC] rounded-lg text-white mt-10 px-4" disabled={!stripe}>Create Payment</Button>
+      <Button type="submit" isLoading={loading} className="bg-[#6D00CC] rounded-lg text-white mt-10 px-4" disabled={!stripe}>Create Payment</Button>
       </div>
     </form>
   );
