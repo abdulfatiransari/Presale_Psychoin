0fd161e63c01f9331afc2853a02f77af77699fa5
Huzaifa Ahmed <huzaifaahmed1182@gmail.com>
2024-04-22 20:13:02 +0500

    feat: edit whitepaper section

--- components/sections/OwnWorld.tsx
+++ components/sections/OwnWorld.tsx
@@ -4,14 +4,16 @@ import React from "react";
 const OwnWorld = () => {
   return (
     <div id="whitepaper" className="flex flex-col px-[280px] justify-center  items-center mt-14 max-sm:px-8 max-md:px-10 max-lg:px-1 mb-14">
-      <h2 className="text-[48px] font-semibold text-white text-center mb-2 max-sm:text-[28px] max-md:text-[32px] ">Shape Your Community</h2>
+      <h2 className="text-[48px] font-semibold text-white text-center mb-2 max-sm:text-[28px] max-md:text-[32px] ">Discover The Insight In Our Whitepaper!</h2>
       <span className="text-[18px] text-center text-[#CDCDCD] mb-6 ">
-        Your stake in Psychoin grants you a voice in our governance. Participate in decisions, engage in community challenges, and contribute to a brighter future for all members.      </span>
-      {/* <div className="flex justify-center items-center mt-4">
-        <Button className="font-semibold flex text-white bg-[#6D00CC] rounded-[100px] px-8 py-6 text-base">
-          Find us on UniSwap
+        Your stake in Psychoin grants you a voice in our governance. Participate in decisions, engage in community challenges, and contribute to a brighter future for all members.</span>
+      <div className="flex justify-center items-center mt-4">
+        <a href="/pdf/whitepaper.pdf" target="_blank" rel="noopener noreferrer">
+        <Button className="font-semibold flex text-white bg-[#6D00CC] rounded-[100px] px-8 py-6 text-base" href="/pdf/whitepaper.pdf" target="_blank">
+        Whitepaper
         </Button>
-      </div> */}
+        </a>
+      </div>
     </div>
   );
 };
